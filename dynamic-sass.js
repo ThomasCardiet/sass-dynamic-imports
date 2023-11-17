c378bb46c9467a3de39fe7776e9f3b02f90b95c6
Thomas Cardiet <thom_car@yahoo.fr>
2023-11-17 16:46:20 +0100

    feat: update dynamic import

--- dynamic-sass.js
+++ dynamic-sass.js
@@ -19,13 +19,23 @@ function compile() {
 
 // Fonction pour lire et extraire les chemins de globbing des déclarations @import
 function getGlobPathsFromImports(fileContent) {
-    const importRegex = /@import\s+['"]([^'"]+\*\*\/\*.scss)['"];/g;
+    const importRegex =
+        /@import\s+['"]((?:\.\.\/)*(?:[^'"]+\*{2}\/\*{2}\/)?[^'"]+\.scss)['"];/g;
     let matches;
     let paths = [];
 
     while ((matches = importRegex.exec(fileContent)) !== null) {
         paths.push(matches[1]);
     }
+
+    paths = paths.map((path) => {
+        return path
+            .split("/")
+            .filter((param) => {
+                return !param.includes("*");
+            })
+            .join("/");
+    });
 
     return paths.map((globPath) =>
         path.resolve(path.dirname(source), globPath)
@@ -35,39 +45,32 @@ function getGlobPathsFromImports(fileContent) {
 // Fonction pour initialiser l'observateur de fichiers
 function initializeWatcher() {
     const fileContent = fs.readFileSync(source, "utf8");
-    let globPaths = getGlobPathsFromImports(fileContent);
+    const globPaths = getGlobPathsFromImports(fileContent);
 
     // Initialise chokidar avec le chemin de base et les motifs de globbing extraits
     const watcher = chokidar.watch([source, ...globPaths], {
         persistent: true,
     });
 
-    // Événement pour les changements, les ajouts, les suppressions de fichiers et de dossiers
+    // Événement pour les changements, les ajouts et les suppressions de fichiers
     watcher.on("all", (event, filePath) => {
+        const regex = /\.scss$/;
+
+        if (!regex.test(filePath)) return;
+
         console.log(`Événement ${event} détecté pour le fichier ${filePath}`);
         compile();
 
-        // Si un fichier/dossier est ajouté ou supprimé, réanalyser et mettre à jour l'observateur
-        if (["add", "unlink", "addDir", "unlinkDir"].includes(event)) {
+        // Si un fichier est ajouté ou supprimé, réanalyser et mettre à jour l'observateur
+        if (
+            ["add", "unlink", "addDir", "unlinkDir"].includes(event) &&
+            filePath.match(/\.(scss|sass)$/)
+        ) {
+            watcher.unwatch(globPaths); // Retirer les anciens chemins
             const newGlobPaths = getGlobPathsFromImports(
                 fs.readFileSync(source, "utf8")
             );
-            const pathsToUnwatch = globPaths.filter(
-                (p) => !newGlobPaths.includes(p)
-            );
-            const pathsToWatch = newGlobPaths.filter(
-                (p) => !globPaths.includes(p)
-            );
-
-            // Mettre à jour les chemins surveillés
-            if (pathsToUnwatch.length > 0) {
-                watcher.unwatch(pathsToUnwatch);
-            }
-            if (pathsToWatch.length > 0) {
-                watcher.add(pathsToWatch);
-            }
-
-            globPaths = newGlobPaths; // Mise à jour de la référence globPaths
+            watcher.add(newGlobPaths); // Ajouter les nouveaux chemins
         }
     });
 
--- test/easy-import.js
+++ test/easy-import.js
@@ -1,119 +1,118 @@
-import test from "ava";
-import easyImport from "../";
-import path from "path";
-import postcss from "postcss";
+import test from 'ava';
+import easyImport from '../';
+import path from 'path';
+import postcss from 'postcss';
 
-const msg = (err) => "postcss-import-dynamic-scss: " + err;
+const msg = err => 'postcss-easy-import: ' + err;
 
 // for tests to work with ava >=0.18
 process.chdir(__dirname);
 
 function preprocess(input, output, opts, t) {
-    return postcss([easyImport(opts)])
-        .process(input)
-        .then((result) => {
+    return postcss([ easyImport(opts) ]).process(input)
+        .then(result => {
             t.is(result.css, output);
             t.is(result.warnings().length, 0);
         });
 }
 
-test("should fail on incorrect 'prefix'", (t) => {
+test('should fail on incorrect \'prefix\'', t => {
     t.throws(() => {
         easyImport({
-            prefix: 1,
+            prefix: 1
         });
-    }, msg("'prefix' option should be a string or false"));
+    }, msg('\'prefix\' option should be a string or false'));
 });
 
-test("should not fail on correct 'prefix'", (t) => {
+test('should not fail on correct \'prefix\'', t => {
     t.notThrows(() => {
         easyImport({
-            prefix: "some string",
+            prefix: 'some string'
         });
     });
 
     t.notThrows(() => {
         easyImport({
-            prefix: false,
+            prefix: false
         });
     });
 });
 
-test("should fail on incorrect 'extensions'", (t) => {
+test('should fail on incorrect \'extensions\'', t => {
     const error = msg(
-        "'extensions' option should be string or array of strings"
+        '\'extensions\' option should be string or array of strings'
     );
 
     t.throws(() => {
         easyImport({
-            extensions: 1,
+            extensions: 1
         });
     }, error);
 
     t.throws(() => {
         easyImport({
-            extensions: "",
+            extensions: ''
         });
     }, error);
 
     t.throws(() => {
         easyImport({
-            extensions: [],
+            extensions: []
         });
     }, error);
 
     t.throws(() => {
         easyImport({
-            extensions: [""],
+            extensions: ['']
         });
     }, error);
 });
 
-test("should not fail on correct 'extensions'", (t) => {
+test('should not fail on correct \'extensions\'', t => {
     t.notThrows(() => {
         easyImport({
-            extensions: ".css",
+            extensions: '.css'
         });
     });
 
     t.notThrows(() => {
         easyImport({
-            extensions: [".css", ".scss"],
+            extensions: ['.css', '.scss']
         });
     });
 });
 
-test("should handle glob imports", (t) => {
+test('should handle glob imports', t => {
     return preprocess(
         '@import "./*.css";\n',
-        ".bar {\n    color: green;\n}\n.foo {\n    color: red;\n}\n",
-        { root: path.resolve("./fixtures/integration") },
+        '.bar {\n    color: green;\n}\n.foo {\n    color: red;\n}\n',
+        { root: path.resolve('./fixtures/integration') },
         t
     );
 });
 
-test("should handle module imports", (t) => {
+test('should handle module imports', t => {
     return preprocess(
         '@import "./module/baz.css";\n',
-        ".baz {\n    color: blue;\n}\n",
-        { root: path.resolve("./fixtures/integration") },
+        '.baz {\n    color: blue;\n}\n',
+        { root: path.resolve('./fixtures/integration') },
         t
     );
 });
 
-test("should handle glob and module imports together", (t) => {
+test('should handle glob and module imports together', t => {
     return preprocess(
         '@import "./module/baz.css";\n @import "./*.css";',
-        ".baz {\n    color: blue;\n}\n .bar {\n    color: green;\n}\n .foo {\n    color: red;\n}", // eslint-disable-line max-len
-        { root: path.resolve("./fixtures/integration") },
+        '.baz {\n    color: blue;\n}\n .bar {\n    color: green;\n}\n .foo {\n    color: red;\n}', // eslint-disable-line max-len
+        { root: path.resolve('./fixtures/integration') },
         t
     );
 });
 
-test("should import glob from node_modules", (t) => {
+test('should import glob from node_modules', t => {
     return preprocess(
         '@import "css.globtest/*.css"',
-        ".bar {\n  color: green;\n}\n.foo {\n  color: tomato;\n}",
+        '.bar {\n  color: green;\n}\n.foo {\n  color: tomato;\n}',
         {},
         t
     );
